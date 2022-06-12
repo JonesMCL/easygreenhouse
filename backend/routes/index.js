236aa97605542dd6bdb431ed89cffbe76f6b49b7
Jonas Lackmann <jonaslackmann@Air-von-Jonas.fritz.box>
2022-06-12 19:59:01 +0200

    updated routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
-import users from './user'
-import auth from './auth'
+//import users from './user'
+//import auth from './auth'
+import sensordataCurrent from './sensordataCurrent'
 
 const router = Router()
 
-router.use(users)
-router.use(auth)
+//router.use(users)
+//router.use(auth)
+router.use(sensordataCurrent)
 
 export default router
