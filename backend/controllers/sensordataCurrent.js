103ffe79341abfb1e0cd1d08309a1a399c9cccb6
Jonas Lackmann <jonaslackmann@Air-von-Jonas.fritz.box>
2022-06-19 13:48:58 +0200

    added API functions

--- backend/controllers/sensordataCurrent.js
+++ backend/controllers/sensordataCurrent.js
@@ -11,32 +11,44 @@ const mariaDBpool = mariadb.createPool({
     connectionLimit: 5
 });
 
-//TODO: Erst Bodentemperatur abfragen 
-//"INSERT INTO soilTemp (sensorid,timestamp,messwert) VALUES (?, ?, ?)"
 async function getAverageSoiltemp (req, res) {
 
     let connection;
     try {
         connection = await mariaDBpool.getConnection();
-        // Wie viele Bodentemperatursensoren haben wir im Einsatz? Danach die Limitzahl richten 
         const result = await connection.query("SELECT messwert FROM soilTemp ORDER BY id DESC LIMIT 2");
-        console.log(result); //[ {val: 1}, meta: ... ]
         let average = (parseFloat(result[0].messwert) + parseFloat(result[1].messwert)) / 2;
-        console.log(average.toFixed(1));
-        //TODO auch json response wenn nichts gefunden,...
         return res.json({
-            status: 200, message: '1', result: average.toFixed(1) // result successfully retrieved
+            status: 200, message: '1', result: average.toFixed(1)
           });
-        
-        //const res = await connection.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
-        //console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
 
     } catch (err) {
-        throw err;
+        return res.json({ status: 400, message: 'Could not fetch average soiltemp from database!' });
     } finally {
         if (connection) return connection.end();
     }
-    
+};
+
+async function getSplitSoiltemp (req, res) {
+
+    let connection;
+    try {
+        connection = await mariaDBpool.getConnection();
+        const result = await connection.query("SELECT messwert FROM soilTemp ORDER BY id DESC LIMIT 2");
+        let soilTemps = {
+            "soilTemp1": parseFloat(result[0].messwert),
+            "soilTemp2": parseFloat(result[1].messwert)
+        }
+        let soilTempsJson = JSON.stringify(soilTemps);
+        return res.json({
+            status: 200, message: '1', result: soilTempsJson
+          });
+
+    } catch (err) {
+        return res.json({ status: 400, message: 'Could not fetch single soiltemp values from database!' });
+    } finally {
+        if (connection) return connection.end();
+    }
 };
 
 async function getAverageAirtemp (req, res) {
@@ -45,37 +57,129 @@ async function getAverageAirtemp (req, res) {
     try {
         connection = await mariaDBpool.getConnection(); 
         const result = await connection.query("SELECT messwert FROM airTemp ORDER BY id DESC LIMIT 2");
-        console.log(result); //[ {val: 1}, meta: ... ]
         let average = (parseFloat(result[0].messwert) + parseFloat(result[1].messwert)) / 2;
-        console.log(average.toFixed(1));
-        //TODO auch json response wenn nichts gefunden,...
         return res.json({
-            status: 200, message: '1', result: average.toFixed(1) // result successfully retrieved
+            status: 200, message: '1', result: average.toFixed(1)
           });
-        
-        //const res = await connection.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
-        //console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
 
     } catch (err) {
-        throw err;
+        return res.json({ status: 400, message: 'Could not fetch average airtemp from database!' });
+    } finally {
+        if (connection) return connection.end();
+    }  
+};
+
+async function getSplitAirtemp (req, res) {
+
+    let connection;
+    try {
+        connection = await mariaDBpool.getConnection();
+        const result = await connection.query("SELECT messwert FROM airTemp ORDER BY id DESC LIMIT 2");
+        let airTemps = {
+            "airTemp1": parseFloat(result[0].messwert),
+            "airTemp2": parseFloat(result[1].messwert)
+        }
+        let airTempsJson = JSON.stringify(airTemps);
+        return res.json({
+            status: 200, message: '1', result: airTempsJson
+          });
+
+    } catch (err) {
+        return res.json({ status: 400, message: 'Could not fetch single airtemp values from database!' });
     } finally {
         if (connection) return connection.end();
     }
-    
-  };
+};
 
-//BENÖTIGTE FUNKTIONEN:
-// SIEHE Dokument von Benedikt 
-// Sende NaN als Wert, wenn keiner verfügbar -> Dann wird in chart.js der Wert ausgelassen 
+async function getAverageSoilMoist (req, res) {
 
-// GET für airtempcurrent 
-// -> aktuelle Temperatur mit Grad C zeichen dahinter; Farbe wird im Frontend definiert 
-// -> Returns: zb 11,8 °C
+    let connection;
+    try {
+        connection = await mariaDBpool.getConnection();
+        const result = await connection.query("SELECT messwert FROM soilMoist ORDER BY id DESC LIMIT 3");
+        let average = (parseFloat(result[0].messwert) + parseFloat(result[1].messwert) + 
+                      parseFloat(result[2].messwert)) / 3;
+        return res.json({
+            status: 200, message: '1', result: average.toFixed(1)
+          });
 
-// GET für airhumidcurrent 
-// -> 
+    } catch (err) {
+        return res.json({ status: 400, message: 'Could not fetch average soil moisture from database!' });
+    } finally {
+        if (connection) return connection.end();
+    }   
+};
+
+async function getSplitSoilMoist (req, res) {
+
+    let connection;
+    try {
+        connection = await mariaDBpool.getConnection();
+        const result = await connection.query("SELECT messwert FROM soilMoist ORDER BY id DESC LIMIT 3");
+        let soilMoist = {
+            "soilMoisture1": parseFloat(result[0].messwert),
+            "soilMoisture2": parseFloat(result[1].messwert),
+            "soilMoisture3": parseFloat(result[2].messwert)
+        }
+        let soilMoistJson = JSON.stringify(soilMoist);
+        return res.json({
+            status: 200, message: '1', result: soilMoistJson
+          });
+
+    } catch (err) {
+        return res.json({ status: 400, message: 'Could not fetch single soil moisture values from database!' });
+    } finally {
+        if (connection) return connection.end();
+    }
+};
+
+async function getAverageHumidity (req, res) {
+
+    let connection;
+    try {
+        connection = await mariaDBpool.getConnection(); 
+        const result = await connection.query("SELECT messwert FROM humidity ORDER BY id DESC LIMIT 2");
+        let average = (parseFloat(result[0].messwert) + parseFloat(result[1].messwert)) / 2;
+        return res.json({
+            status: 200, message: '1', result: average.toFixed(1)
+          });
+
+    } catch (err) {
+        return res.json({ status: 400, message: 'Could not fetch average humidity from database!' });
+    } finally {
+        if (connection) return connection.end();
+    }  
+};
+
+async function getSplitHumidity (req, res) {
+
+    let connection;
+    try {
+        connection = await mariaDBpool.getConnection();
+        const result = await connection.query("SELECT messwert FROM humidity ORDER BY id DESC LIMIT 2");
+        let soilMoist = {
+            "humidity1": parseFloat(result[0].messwert),
+            "humidity2": parseFloat(result[1].messwert)
+        }
+        let soilMoistJson = JSON.stringify(soilMoist);
+        return res.json({
+            status: 200, message: '1', result: soilMoistJson
+          });
+
+    } catch (err) {
+        return res.json({ status: 400, message: 'Could not fetch single humidity values from database!' });
+    } finally {
+        if (connection) return connection.end();
+    }
+};
 
 export default {
     getAverageSoiltemp,
-    getAverageAirtemp
+    getSplitSoiltemp,
+    getAverageAirtemp,
+    getSplitAirtemp,
+    getAverageSoilMoist,
+    getSplitSoilMoist,
+    getAverageHumidity,
+    getSplitHumidity
 }
--- backend/routes/sensordataCurrent.js
+++ backend/routes/sensordataCurrent.js
@@ -3,21 +3,37 @@ import sensorCtrl from '../controllers/sensordataCurrent'
 
 const router = Router()
 
-
-//TODO: AKTUALISIEREN
-//router.route('/sensordata/airtempCurrent')
-  /** GET /api/users/:userId - Get single */
-//  .get(userCtrl.getUser)
-
 router.route('/sensordataCurrent/getAverageSoiltemp')
-  /** GET /api/sensordata/getAverageSoiltempCurrent - Get average soil temperature */
+  /** GET /api/sensordata/getAverageSoiltemp - Get average soil temperature */
   .get(sensorCtrl.getAverageSoiltemp)
 
-//router.route('/sensordata/getSplitSoiltempCurrent')
+router.route('/sensordataCurrent/getSplitSoiltemp')
+  /** GET /api/sensordata/getSplitSoiltemp - Get single sensor soil temperatures */
+  .get(sensorCtrl.getSplitSoiltemp)
 
 router.route('/sensordataCurrent/getAverageAirtemp')
-  /** GET /api/sensordata/getAverageAirtempCurrent - Get average air temperature */
+  /** GET /api/sensordata/getAverageAirtemp - Get average air temperature */
   .get(sensorCtrl.getAverageAirtemp)
+
+router.route('/sensordataCurrent/getSplitAirtemp')
+  /** GET /api/sensordata/getSplitAirtemp - Get single sensor air temperatures */
+  .get(sensorCtrl.getSplitAirtemp)
+
+router.route('/sensordataCurrent/getAverageSoilMoist')
+  /** GET /api/sensordata/getAverageSoiltemp - Get average soil moisture value */
+  .get(sensorCtrl.getAverageSoilMoist)
+
+router.route('/sensordataCurrent/getSplitSoilMoist')
+  /** GET /api/sensordata/getSplitSoilMoist - Get single sensor soil moisture values */
+  .get(sensorCtrl.getSplitSoilMoist)
+
+router.route('/sensordataCurrent/getAverageHumidity')
+  /** GET /api/sensordata/getAverageHumidity - Get average humidity */
+  .get(sensorCtrl.getAverageHumidity)
+
+router.route('/sensordataCurrent/getSplitHumidity')
+  /** GET /api/sensordata/getSingleHumidity - Get single sensor humidities */
+  .get(sensorCtrl.getSplitHumidity)
 
   
 //EXAMPLES: 
