80e153bc6dd983094dbdf2a505afa2f5bee07406
Jonas Lackmann <jonaslackmann@Air-von-Jonas.fritz.box>
2022-06-19 16:14:36 +0200

    cleaned up index.js and deleted unnecessary lines

--- backend/index.js
+++ backend/index.js
@@ -1,56 +1,17 @@
 const express = require("express");
 const cors = require('cors');
 const chalk = require('chalk');
-const redis = require("redis");
 const app = express();
 const server = require('http').createServer(app);
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
 import ApiRoutes from './routes';
-
 let constants = require('./config/config.js');
-const client = redis.createClient({
-    host: constants.REDISHOST,
-    port: constants.REDISPORT,
-    password: constants.REDISPWD,
-    retry_strategy: function(options) {
-      if (options.error && options.error.code === "ECONNREFUSED") {
-        // End reconnecting on a specific error and flush all commands with
-        // a individual error
-        return new Error("The server refused the connection");
-      }
-      if (options.total_retry_time > 1000 * 60 * 60) {
-        // End reconnecting after a specific timeout and flush all commands
-        // with a individual error
-        return new Error("Retry time exhausted");
-      }
-      if (options.attempt > 10) {
-        // End reconnecting with built in error
-        return undefined;
-      }
-      // reconnect after
-      return Math.min(options.attempt * 100, 3000);
-    }
-});
 
 app.use(cors())
 
 // Add the ApiRoutes stack to the server
 app.use('/api', ApiRoutes)
-
-client.on("error", (err) => {
-    console.log(err);
-})
-
-app.get('/', cors(), (req, res)=> {
-    try {
-        client.get("ping", function(err, reply) {
-            res.send(reply.toString());
-        })
-    } catch(err) {
-        res.status(500).send({message: err.message});
-    }
-});
 
 server.listen(constants.APPLICATIONPORT, err => {
     if (err) {
